6fdf5c52d8acd9ebcb2bf1bc04de216b36c91f7d
Miguelcomolacerveza <113274338+Miguelcomolacerveza@users.noreply.github.com>
2023-04-24 23:44:52 +0200

    Update Ejercicio1Prestamos.sol

--- Ejercicio1Prestamos.sol
+++ Ejercicio1Prestamos.sol
@@ -1,15 +1,37 @@
 
+
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity ^0.8.19;
+
+/*
+ This contract is made to provide loans to customer through a "bank".
+*/
  
- 
- // Contract in process..... Sorry for the inconvinience ;)
  contract LoanBase {
 
-     error notEnoughFunds();
+    address owner;
+
+     constructor() {
+         owner = msg.sender;
+     }
+
+     error NoAccessToThisAccount();
 
      event loanCreated();
      event loanApproved();
+
+     enum Status {
+         PENDING,
+         APPROVED,
+         CANCELED
+     }
+
+     // @param provider -> address of the provider of the loan
+     // @param borrower -> address of the borrower of the loan
+     // @param amount -> full amount of the requested loan
+     // @param loanInstallment -> amount of each single payment to repay the loan
+     // @param expirationDate -> Deadline to repay the loan
+     // @param status -> Status type var used as a container of the status of the loan
 
      struct Loan {
          address provider;
@@ -17,28 +39,27 @@ pragma solidity ^0.8.19;
          uint256 amount;
          uint256 loanInstallment;
          uint256 expirationDate;
-         bool approved;    
+         Status status;
      }
 
-     modifier onlyProvider(uint _loanID) {
-      require(msg.sender == loans[_loanID].provider);
+     modifier onlyOwner(uint _loanID) {
+      require(msg.sender == owner);
       _;
      }
-
-     mapping(address => uint256) balances;
-     mapping(uint256 => Loan) loans;
-     uint256 loanID;
+     
+     mapping(address => uint256) balances; // mapping with all the customer´s balances
+     mapping(uint256 => Loan) loans; // mapping with all the customer´s loans
+     uint256 loanID; // Counter to identify each loan
 
      function setLoan(
-         address _borrower,
          uint256 _amount,
          uint256 _loanInstallment,
          uint256 _expirationDate
      ) public {
-         loans[loanID].provider = _borrower;
+         loans[loanID].borrower = msg.sender;
          loans[loanID].amount = _amount;
          loans[loanID].loanInstallment = _loanInstallment;
-         loans[loanID].expirationDate = _expirationDate;
+         loans[loanID].expirationDate = _expirationDate * 86400;
          loanID++;
 
          emit loanCreated();
@@ -48,14 +69,22 @@ pragma solidity ^0.8.19;
          return loans[_loanID];
      }
 
-     function approveLoan(uint256 _loanID) public onlyProvider(_loanID) {
-         loans[_loanID].approved = true;
-         emit loanApproved();
+     function approveLoan(uint256 _loanID) public onlyOwner(_loanID) {
+         // Once the bank has approved the loan, we:
 
+         // Modify the status to approved
+         loans[_loanID].status = Status.APPROVED;
+         // As the bank has approved the loan, we set the expiration date based on this action
+         loans[_loanID].expirationDate += block.timestamp;
+         // Set the address of the bank as a provider
+         loans[_loanID].provider = owner;
+         // Add the loan amount to the customer´s account
+         balances[loans[_loanID].borrower] += loans[_loanID].amount;
+         emit loanApproved();
      }
 
-     function calculatePercentage(uint256 _percentage, uint256 _amount) public pure returns(uint256) {
-         uint256 totalPercentage = (_percentage * _amount) / 100;
-         return totalPercentage;
+     function getBalance(address _account) public view returns(uint256) {
+         if(msg.sender != _account) revert NoAccessToThisAccount();
+         return balances[_account];
      }
  }
